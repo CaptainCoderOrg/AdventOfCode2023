ba1e4e28202a38796445f2035d0d962958e2a2b8
Joseph Collard <jcollard@users.noreply.github.com>
2023-12-13 06:19:37 -0700

    feat: Implement Day 13 Part 1

---
+++ 13/Implementation/Day13.cs
@@ -0,0 +1,75 @@
+using System.Text;
+
+public class Day13
+{
+
+    public static long Part1(string[] input)
+    {
+        IEnumerable<Grid> rawGrids = 
+                string.Join(Environment.NewLine, input)
+                .Split($"{Environment.NewLine}{Environment.NewLine}", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+                .Select(g => new Grid(g));
+        
+        int rows = rawGrids.Select(g => g.FindMirrorRow()).Sum();
+        int cols = rawGrids.Select(g => g.FindMirrorColumn()).Sum();
+
+        return rows*100 + cols;
+    }
+
+
+
+}
+
+public record Grid(string RawGrid)
+{
+    public string[] Rows { get; } = RawGrid.Split(Environment.NewLine, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+    public int FindMirrorRow()
+    {
+        for (int row = 0; row < Rows.Length - 1; row++)
+        {
+            (Grid top, Grid bottom) = MirrorRows(row);
+            if (top.RawGrid == bottom.RawGrid) { return row + 1; }
+        }
+        return 0;
+    }
+
+    public int FindMirrorColumn() => Transpose().FindMirrorRow();
+
+    public Grid Transpose()
+    {
+        List<string> newRows = new();
+        for (int col = 0; col < Rows[0].Length; col++)
+        {
+            StringBuilder builder = new();
+            for (int row = 0; row < Rows.Length; row++)
+            {
+                builder.Append(Rows[row][col]);
+            }
+            newRows.Add(builder.ToString());
+            builder.Clear();
+        }
+        return new Grid(string.Join(Environment.NewLine, newRows));
+    }
+
+    public (Grid Left, Grid Right) MirrorColumns(int col)
+    {
+        Grid transposed = Transpose();
+        (Grid Top, Grid Bottom) = transposed.MirrorRows(col);
+        return (Top.Transpose(), Bottom.Transpose());
+    }
+
+
+    public (Grid Top, Grid Bottom) MirrorRows(int row)
+    {
+        string[] top = Rows[..(row + 1)];
+        string[] bottom = Rows[(row + 1)..];
+        top = [.. top.TakeLast(bottom.Length)];
+        bottom = [.. bottom.Take(top.Length).Reverse()];
+        Grid topGrid = new Grid(string.Join(Environment.NewLine, top));
+        Grid bottomGrid = new Grid(string.Join(Environment.NewLine, bottom));
+        return (topGrid, bottomGrid);
+    }
+
+
+}
---
+++ 13/Tests/Day13Tests.cs
@@ -0,0 +1,146 @@
+using Shouldly;
+
+namespace Tests;
+
+public class Day13Tests
+{
+
+    public static readonly string[] SampleInput =
+    """
+    #.##..##.
+    ..#.##.#.
+    ##......#
+    ##......#
+    ..#.##.#.
+    ..##..##.
+    #.#.##.#.
+
+    #...##..#
+    #....#..#
+    ..##..###
+    #####.##.
+    #####.##.
+    ..##..###
+    #....#..#
+    """.Trim().Split(Environment.NewLine, StringSplitOptions.TrimEntries);
+
+    public static readonly string[] PuzzleInput = File.ReadAllLines("input.txt");
+
+    [Fact]
+    public void test_sample_input_part1()
+    {
+        Day13.Part1(SampleInput).ShouldBe(405);
+    }
+
+    [Fact]
+    public void test_puzzle_input_part1()
+    {
+        Day13.Part1(PuzzleInput).ShouldBe(33780L);
+    }
+
+    [Fact]
+    public void test_find_mirror_row()
+    {
+        string rawGrid =
+        """
+        #...##..#
+        #....#..#
+        ..##..###
+        #####.##.
+        #####.##.
+        ..##..###
+        #....#..#
+        """.Trim();
+        Grid grid = new Grid(rawGrid);
+
+        grid.FindMirrorRow().ShouldBe(4);
+    }
+
+    [Fact]
+    public void test_find_mirror_column()
+    {
+        string rawGrid =
+        """
+        #.##..##.
+        ..#.##.#.
+        ##......#
+        ##......#
+        ..#.##.#.
+        ..##..##.
+        #.#.##.#.
+        """.Trim();
+        Grid grid = new Grid(rawGrid);
+
+        grid.FindMirrorColumn().ShouldBe(5);
+    }
+
+    [Fact]
+    public void test_mirror_rows()
+    {
+        string rawGrid =
+        """
+        #...##..#
+        #....#..#
+        ..##..###
+        #####.##.
+        #####.##.
+        ..##..###
+        #....#..#
+        """.Trim();
+        Grid grid = new Grid(rawGrid);
+
+        (Grid top, Grid bottom) = grid.MirrorRows(3);
+
+        top.RawGrid.ShouldBe(bottom.RawGrid);
+    }
+
+    [Fact]
+    public void test_mirror_columns()
+    {
+        string rawGrid =
+        """
+        #.##..##.
+        ..#.##.#.
+        ##......#
+        ##......#
+        ..#.##.#.
+        ..##..##.
+        #.#.##.#.
+        """.Trim();
+        Grid grid = new Grid(rawGrid);
+
+        (Grid left, Grid right) = grid.MirrorColumns(4);
+
+        left.RawGrid.ShouldBe(right.RawGrid);
+
+    }
+
+    [Fact]
+    public void test_transpose_grid()
+    {
+        string rawGrid =
+        """
+        ABCD
+        EFGH
+        IJKL
+        """.Trim();
+        Grid grid = new Grid(rawGrid);
+        Grid transposed = grid.Transpose();
+        string transposedGrid =
+        """
+        AEI
+        BFJ
+        CGK
+        DHL
+        """.Trim();
+        transposed.RawGrid.ShouldBe(transposedGrid);
+    }
+
+    // [Fact]
+    // public void test_grid_equality()
+    // {
+    //     Grid g0 = new Grid("ABC\nDEF");
+    //     Grid g1 = new Grid("ABC\nDEF");
+    //     g0.ShouldBe(g1);
+    // }
+}
