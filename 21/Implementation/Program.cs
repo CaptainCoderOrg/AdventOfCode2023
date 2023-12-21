6a3088b8a397ff382d72dd3ed7636c4ea516103c
Joseph Collard <jcollard@users.noreply.github.com>
2023-12-21 12:19:56 -0700

    chore: Discovered there will be either 13 or 14 patterns in puzzle input.

--- 21/Implementation/Program.cs
+++ 21/Implementation/Program.cs
@@ -5,11 +5,10 @@ using AoCHelpers;
 Map map = Map.Parse(Day21.PuzzleInput);
 // map.MarkAndPrintPositions(map.Start, 66);
 // map.MarkAndPrintPositions(map.Start, 30, 3);
-int radius = 5;
-int steps = 500;
+int radius = 8;
+int steps = (map.Rows/2) + map.Rows * (radius - 1);
 HashSet<Position> marks = map.MarkPositions(map.Start, steps, radius);
-map.PrintMarks(marks, radius);
-// map.PrintSubGrid(1, 0, marks);
+string emptyPattern = map.SubGridToString(0, 0, new ());
 
 
 Dictionary<string, List<Position>> patterns = new ();
@@ -20,6 +19,7 @@ for (int row = -radius + 1; row < radius; row++ )
     {
         Position subGrid = (row, col);
         string subGridPattern = map.SubGridToString(row, col, marks);
+        if (subGridPattern == emptyPattern) { continue; } // Skip grids we did not enter
         if (!patterns.TryGetValue(subGridPattern, out List<Position>? found))
         {
             found = new List<Position>();
@@ -29,7 +29,16 @@ for (int row = -radius + 1; row < radius; row++ )
     }
 }
 
+Console.WriteLine($"Total Patterns: {patterns.Keys.Count}");
 foreach ((string pattern, List<Position> subGrid) in patterns)
 {
     Console.WriteLine($"Match: {string.Join(", ", subGrid.Select(r => $"({r.Row}, {r.Col})"))}");
 }
+
+Console.WriteLine();
+// map.PrintMarks(marks, radius);
+// map.PrintSubGrid(1, 0, marks);
+
+// int elfSteps = 26501365;
+// int stepsToEdge = map.Rows/2;
+
